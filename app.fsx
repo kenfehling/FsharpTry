1cd9a2e1a2ae54deed7842e3a76e4ec57d5d437a
Ken Fehling <ken@androidideas.org>
2016-06-21 02:26:49 -0400

    choose

--- app.fsx
+++ app.fsx
@@ -12,15 +12,13 @@ open Suave.Successful
 open FSharp.Data
 open ForecastIO
 
-let request = new ForecastIORequest("8fa1a8798aaafef8dabd413af871819c", 37.8267f, -122.423f, Unit.si);
-let response = request.Get();
+//let request = new ForecastIORequest("8fa1a8798aaafef8dabd413af871819c", 37.8267f, -122.423f, Unit.si);
+//let response = request.Get();
 
-let app ctx = async {
+let app ctx =
   choose
     [ GET >=> choose
         [ path "/webhook" >=> OK "Hello GET"
           path "/" >=> OK "Hello World" ]
       POST >=> choose
         [ path "/webhook" >=> OK "Hello POST"] ]
-
-  return! ctx |> Successful.OK(html) }
