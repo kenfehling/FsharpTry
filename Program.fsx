5d5da52b1ddfe19bc15a06e48d39aede10c25969
Ken Fehling <ken@androidideas.org>
2016-06-21 00:37:41 -0400

    First commit

---
+++ Program.fsx
@@ -0,0 +1,18 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open ForecastIO
+
+let request = new ForecastIORequest("8fa1a8798aaafef8dabd413af871819c", 37.8267f, -122.423f, Unit.si);
+let response = request.Get();
+
+let app =
+  choose
+    [ GET >=> choose
+        [ path "/webhook" >=> OK "Hello GET"
+          path "/" >=> OK "Hello World" ]
+      POST >=> choose
+        [ path "/webhook" >=> OK "Hello POST"] ]
+
+startWebServer defaultConfig app
